9491b7b0140171b3a8c533abde11df685f5a7e73
Sriram MK <mksriram@umich.edu>
2024-02-13 14:55:35 -0500

    added env files, coded basic

--- main.py
+++ main.py
@@ -1,20 +1,55 @@
+import os
+from dotenv import load_dotenv
 import discord
-from discord.ext import commands 
-intents = discord.Intents.default() 
-intents.message_content = True 
-client = commands.Bot(command_prefix="!", intents=intents)
+from discord.ext import commands
+import asyncio
+#loading env file
+
+load_dotenv() 
+
+#retriving the token
+DISCORD_API_TOKEN = os.getenv("DISCORD_API_TOKEN")
+print(type(DISCORD_API_TOKEN))
 
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+def run(): 
+    intents = discord.Intents.default() 
+    bot = commands.Bot(command_prefix="!", intents=intents)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    @bot.event
+    async def on_ready():
+        print(bot.user)
+        print(bot.user.id)
+        print("--------------------------")
+        
+        
+    @bot.command
+    async def say(ctx, what): 
+        await ctx.send(what)
+    
+    
+    # if (len(message.attachments) != 0): 
+    @bot.event 
+    async def on_message(message):
+        if message.author == bot.user:
+            return
+        
+       
+        listSniped = message.mentions
+        for i in listSniped:
+            print(i)
+        print(listSniped)
+        sniper_id = message.author.id
+        sender = message.author.display_name
+        await message.channel.send(f"{sender} is the GOAT")
+        
+        
+        
+        
+        
+        
+    bot.run(DISCORD_API_TOKEN) #remove this and ENV it later 
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
 
-client.run('MTIwNTMxNzUyMDQwNDUyMTAyMA.GqUNF3.M43r5FiH7-VW53wc_h3UHCfPbanxTPBdNAZ3w8')
+if __name__ == "__main__":
+    run() 
